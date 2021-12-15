806b5f3d12a38dbf72735141a73ab6e94e0857d7
João Sass <joao.sass@tangotech.com.br>
2021-12-15 17:32:21 -0300

    change getMovies url

--- src/utils/getMovies.ts
+++ src/utils/getMovies.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const getMovies = async (movieId: string = '') => {
   try {
-    const result = await axios.get(`https://swapi.dev/api/films/${movieId}`);
-    return movieId ? result.data : result.data.results;
+    const result = await axios.get(`https://swapi-deno.azurewebsites.net/api/films/${movieId}`);
+    // return movieId ? result.data : result.data.results;
+    return result.data;
   } catch (err) {
     return false;
   }
